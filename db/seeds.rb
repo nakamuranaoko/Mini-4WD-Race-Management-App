d59f8d2944971820f7c46b5598b56587573b6f94
nakamuranaoko <wara1203wara_pet@yahoo.co.jp>
2024-11-26 16:31:32 +0900

    ダミーデータ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,42 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# db/seeds.rb
+
+# ユーザーの作成
+user = User.create!(
+  email: "test@example.com",
+  password: "password",
+  name: "Test User"
+)
+
+# イベントデータを作成
+5.times do |i|
+  event = Event.create!(
+    date: Date.today - i,
+    event_name: "大会 #{i + 1}",
+    venue: "会場 #{i + 1}",
+    weather: ["晴れ", "曇り", "雨"].sample,
+    temperature: rand(15..30),
+    coment: "大会 #{i + 1} のコメントです。",
+    user: user
+  )
+
+  # タイムデータを作成
+  2.times do |j|
+    RaceTime.create!(
+      rap_time: rand(10.0..30.0).round(2),
+      event: event
+    )
+  end
+
+#   # 写真データを作成
+#   3.times do
+#     CorusePhoto.create!(
+#       image_url: "https://via.placeholder.com/300", # プレースホルダー画像
+#       event: event
+#     )
+#   end
+end
+
+puts "ダミーデータの作成が完了しました！"
