1a6f1016dec8d0dde8311c76e32a6c38a04f7491
nakamuranaoko <wara1203wara_pet@yahoo.co.jp>
2024-11-27 14:14:13 +0900

    add:レース記録作成画面

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,11 @@
 class EventsController < ApplicationController
+  # レース一覧
   def index
     @events = Event.includes(:user, :race_times).order(date: :desc)
+  end
+
+  # レース記録作成
+  def new
+    @events = Event.new
   end
 end
