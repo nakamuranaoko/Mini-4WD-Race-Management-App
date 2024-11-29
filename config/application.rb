5e20b0529ca0ca58f39451c82609f6251aba15d3
nakamuranaoko <wara1203wara_pet@yahoo.co.jp>
2024-11-29 19:44:33 +0900

    fix:エラー修正3

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require_relative "boot"
 
 require "rails/all"
-require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+# require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
 
 
 # Require the gems listed in Gemfile, including any gems
@@ -18,8 +18,8 @@ module Myapp
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    # config.autoload_lib(ignore: %w[assets tasks])
-    config.autoload_paths += %W(#{config.root}/lib)
+    config.autoload_lib(ignore: %w[assets tasks])
+    # config.autoload_paths += %W(#{config.root}/lib)
 
     # Configuration for the application, engines, and railties goes here.
     #
