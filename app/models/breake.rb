0fe75687db2369625411e75adf1c0717bffa60e8
nakamuranaoko <wara1203wara_pet@yahoo.co.jp>
2024-11-29 20:47:31 +0900

    fix:enum_color修正

--- app/models/breake.rb
+++ app/models/breake.rb
@@ -2,5 +2,5 @@ class Breake < ApplicationRecord
   belongs_to :machine
 
   enum name: { front: 0, side: 1, rear: 2 } # 位置をEnumで管理
-  enum color: { green: 0, blue: 1, black: 2, gray: 3, white: 4, pink: 5, none: 6 } # カラー
+  enum color: { green: 0, blue: 1, black: 2, gray: 3, white: 4, pink: 5, no_color: 6 } # カラー
 end
