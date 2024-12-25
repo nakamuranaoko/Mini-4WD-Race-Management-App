f97aa8d209874300e4ddb4780047c46519079888
nakamuranaoko <wara1203wara_pet@yahoo.co.jp>
2024-12-26 00:34:51 +0900

    fix:コード修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,17 +5,6 @@ module.exports = {
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
   ],
-  plugins: [require("daisyui")],
-  daisyui: {
-    darkTheme: false, // ダークモードをONにする場合は削除
-  },
-}module.exports = {
-  content: [
-    './app/views/**/*.html.erb',
-    './app/helpers/**/*.rb',
-    './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
-  ],
   theme: {
     extend: {
       colors: {
