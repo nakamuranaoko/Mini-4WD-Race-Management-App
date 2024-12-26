70cb15d978c4794428494e46b6d8586c3fae7d21
nakamuranaoko <wara1203wara_pet@yahoo.co.jp>
2024-12-27 01:04:59 +0900

    add:creat_linkform

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_25_052324) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_26_140116) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -46,6 +46,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_25_052324) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "link"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
