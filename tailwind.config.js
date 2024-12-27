8e615c579df54fe5444c7f3f928b0af2179347a2
nakamuranaoko <wara1203wara_pet@yahoo.co.jp>
2024-12-27 23:53:35 +0900

    update:search_form

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,17 +5,6 @@ module.exports = {
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
   ],
-  theme: {
-    extend: {
-      colors: {
-        tagBackground: '#3b82f6', // タグの背景色
-        tagHover: '#2563eb',      // ホバー時の背景色
-      },
-      borderRadius: {
-        full: '9999px',          // 完全な丸みを追加
-      },
-    },
-  },
   plugins: [require("daisyui")],
   daisyui: {
     darkTheme: false, // ダークモードをONにする場合は削除
