38575565886e2cf7cc7fedfeaed5d87c3b442360
nakamuranaoko <wara1203wara_pet@yahoo.co.jp>
2025-01-09 03:53:33 +0900

    fix:エラー修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,11 +43,11 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
   config.action_mailer.delivery_method = :smtp
-  
+
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "localhost",
+    domain: "gmai.com",
     user_name: ENV["MAILER_SENDER"],
     password: ENV["MAILER_PASSWORD"],
     authentication: "plain",
