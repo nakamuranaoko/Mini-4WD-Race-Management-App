dd4393c4ad20a11f2d2a6c19dc6a2beda864fd3b
nakamuranaoko <wara1203wara_pet@yahoo.co.jp>
2025-01-10 04:11:12 +0900

    fix:環境設定編集

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,13 +41,15 @@ Rails.application.configure do
   # caching is enabled.
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true # メール送信を有効化
+  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  
 
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "gmai.com",
+    domain: "gmail.com",
     user_name: ENV["MAILER_SENDER"],
     password: ENV["MAILER_PASSWORD"],
     authentication: "plain",
