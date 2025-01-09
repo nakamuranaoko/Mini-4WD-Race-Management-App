deab298185f20d8b149ab0d09a07c17217567250
nakamuranaoko <wara1203wara_pet@yahoo.co.jp>
2025-01-10 01:47:47 +0900

    add:メール承認機能を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable
+         :omniauthable, :confirmable
 
   has_many :events, dependent: :destroy # ユーザーが削除された時、Eventレコードも削除される
 end
