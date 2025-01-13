a95600b23788a1725ac0b01b5d8dc181e12528da
nakamuranaoko <wara1203wara_pet@yahoo.co.jp>
2025-01-14 03:26:51 +0900

    update:google_login

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,22 @@ class User < ApplicationRecord
   # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable, :confirmable
+         :confirmable, :omniauthable, omniauth_providers: [:google_oauth2]
 
   has_many :events, dependent: :destroy # ユーザーが削除された時、Eventレコードも削除される
+
+  validates :uid, presence: true, uniqueness: { scope: :provider }, if: -> { uid.present? }
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.name = auth.info.name
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+    end
+  end
+
+  def self.create_unique_string
+    SecureRandom.uuid
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_09_161935) do
+ActiveRecord::Schema[7.2].define(version: 2025_01_13_145604) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -158,9 +158,12 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_09_161935) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.string "provider"
+    t.string "uid"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
   add_foreign_key "breakes", "machines"
