ae8ef24c46864bf815463df4038ef3049e243ed5
nakamuranaoko <wara1203wara_pet@yahoo.co.jp>
2025-01-14 13:42:18 +0900

    update:Cookieを設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,18 @@ class ApplicationController < ActionController::Base
   allow_browser versions: :modern
   # nameを保存できるように変更する。
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_cookie # クッキーを設定するメソッドを呼び出す
+
+  private
+
+  def set_cookie
+    cookies[:my_cookie] = {
+      value: "value_here",
+      secure: true, # HTTPS通信でのみCookieを送信
+      same_site: :none # サードパーティクッキーを許可
+    }
+  end
+
 
   protected
 
