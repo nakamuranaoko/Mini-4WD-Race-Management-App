1502c2cc2ddffcb6d132f0a3cf46c73386e3594a
nakamuranaoko <wara1203wara_pet@yahoo.co.jp>
2025-01-14 22:48:53 +0900

    fix:favicon修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,20 +5,3 @@ Rails.start();
 
 import "@hotwired/turbo-rails"
 import "./controllers"
-
-document.addEventListener("DOMContentLoaded", () => {
-  const dateInput = document.querySelector("input[name='event[date]']");
-  if (dateInput) {
-    dateInput.addEventListener("input", (event) => {
-      const value = event.target.value;
-
-      // "YYYYMMDD" 形式を検証
-      if (!/^\d{8}$/.test(value)) {
-        event.target.setCustomValidity("日付はYYYYMMDD形式で入力してください (例: 20250114)");
-      } else {
-        event.target.setCustomValidity("");
-      }
-    });
-  }
-});
-  
