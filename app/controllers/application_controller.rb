e91e2efb9d6c896b46b844077bc07e8163a8d90d
nakamuranaoko <wara1203wara_pet@yahoo.co.jp>
2025-01-14 16:15:30 +0900

    fix:クッキーの警告に対するコード

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,17 +3,18 @@ class ApplicationController < ActionController::Base
   allow_browser versions: :modern
   # nameを保存できるように変更する。
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :set_cookie # クッキーを設定するメソッドを呼び出す
+  before_action :set_custom_cookie # クッキーを設定するメソッドを呼び出す
 
   private
 
-  def set_cookie
+  def set_custom_cookie
     cookies[:my_cookie] = {
       value: "value_here",
-      secure: true, # HTTPS通信でのみCookieを送信
+      secure: Rails.env.production?, # 本番環境ではHTTPSを使用
       same_site: :none # サードパーティクッキーを許可
     }
   end
+
 
 
   protected
