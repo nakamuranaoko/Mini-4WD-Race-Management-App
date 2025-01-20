ac36f9726867a51fdcf85fefb25882faa2dbcc28
nakamuranaoko <wara1203wara_pet@yahoo.co.jp>
2025-01-20 15:57:44 +0900

    fix:ogpの内容修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,11 +4,16 @@ module ApplicationHelper
     display_meta_tags
   end
 
+  def page_title(title = '')
+    base_title = 'MINI4NOTE'
+    title.present? ? "#{title} | #{base_title}" : base_title
+  end
+
   def assign_meta_tags(options = {})
     defaults = t('meta_tags.defaults')
     options.reverse_merge!(defaults)
-    site = options[:site]
-    title = options[:title]
+    site = options[:site] || 'MINI4NOTE'
+    title = options[:title].presence || site
     description = options[:description]
     keywords = options[:keywords]
     image = options[:image].presence || image_url('placeholder.png')
@@ -25,7 +30,7 @@ module ApplicationHelper
       },
       og: {
         type: 'website',
-        title: title.presence || site,
+        title: "#{title} | #{site}",
         description:,
         url: request.original_url,
         image:,
