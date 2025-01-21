7de9be29826080e3f9b62763abb612ae839d715b
nakamuranaoko <wara1203wara_pet@yahoo.co.jp>
2025-01-21 09:36:09 +0900

    update:タイムスタンプ追加と画像リサイズ

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,21 +4,21 @@ module ApplicationHelper
     display_meta_tags
   end
 
-  def page_title(title = '')
-    base_title = 'MINI4NOTE'
+  def page_title(title = "")
+    base_title = "MINI4NOTE"
     title.present? ? "#{title} | #{base_title}" : base_title
   end
 
   def assign_meta_tags(options = {})
-    defaults = t('meta_tags.defaults')
+    defaults = t("meta_tags.defaults")
     options.reverse_merge!(defaults)
-    site = options[:site] || 'MINI4NOTE'
+    site = options[:site] || "MINI4NOTE"
     title = options[:title].presence || site
     description = options[:description]
     keywords = options[:keywords]
-    image = options[:image].presence || image_url('placeholder.png')
+    image = options[:image].presence || image_url("placeholder.png")
     configs = {
-      separator: '|',
+      separator: "|",
       reverse: true,
       site:,
       title:,
@@ -26,10 +26,10 @@ module ApplicationHelper
       keywords:,
       canonical: request.original_url,
       icon: {
-        href: image_url('favicon.ico')
+        href: image_url("favicon.ico")
       },
       og: {
-        type: 'website',
+        type: "website",
         title: "#{title} | #{site}",
         description:,
         url: request.original_url,
@@ -38,7 +38,7 @@ module ApplicationHelper
       },
       twitter: {
         site:,
-        card: 'summary_large_image',
+        card: "summary_large_image",
         image:
       }
     }
@@ -46,8 +46,5 @@ module ApplicationHelper
 
     puts "options[:title]: #{options[:title].inspect}"
     puts "title before assignment: #{title}"
-
   end
-
-  
 end
